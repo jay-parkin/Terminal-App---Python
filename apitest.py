d3db72125c07c1b723c26f9df854eebe94f61e2c
jay-parkin <jay-parkin91@hotmail.com>
2024-04-22 23:24:13 +1000

    gitignore changes

--- apitest.py
+++ apitest.py
@@ -6,7 +6,7 @@ querystring = {"prefix":"chicken breast"}
 
 headers = {
 	"X-RapidAPI-Key": "0bccf89588mshcb9bb8398bc676bp1acf33jsn1a95f7f435b4",
-	"X-RapidAPI-Host": "tasty.p.rapidapi.com"
+	"X-RapidAPI-Host": "tasty.p.rapidapi.comw"
 }
 
 response = requests.get(url, headers=headers, params=querystring)
